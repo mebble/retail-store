dcadfb5e0a2b3e569e94f93c1cf5e44260d00c0b
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2019-03-11 17:43:55 +0530

    basic gui template

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'hbs');
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 // app.use(bodyParser.urlencoded({
 //     extended: true
 // }));
